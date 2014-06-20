ca7cb3e457298a70d92895cf6b9fb175094d25e3
MorphanOne <morphanone@gmail.com>
2014-06-20 19:25:25 -0400

    Clean Heroes events, add dHero object

--- src/main/java/net/gnomeffinway/depenizen/events/HeroesEvents.java
+++ src/main/java/net/gnomeffinway/depenizen/events/HeroesEvents.java
@@ -12,6 +12,7 @@ import net.aufdemrand.denizen.objects.dPlayer;
 import net.aufdemrand.denizen.objects.dObject;
 import net.gnomeffinway.depenizen.Depenizen;
 
+import net.gnomeffinway.depenizen.objects.dHero;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -31,33 +32,37 @@ public class HeroesEvents implements Listener {
     // hero changes class (to <class>)
     // @Triggers when a Hero changes classes.
     // @Context
-    // <context.class> returns the class that the Hero changed to.
+    // <context.class> returns the class that the Hero is changing to.
     // <context.cost> returns the cost of changing the class.
-    // <context.entity> returns the Hero changing classes.
+    // <context.from> returns the class that the Hero is changing from.
+    // <context.hero> returns the Hero changing classes.
+    // @Determine
+    // "CANCELLED" to stop the hero from changing classes.
     // @Plugin Heroes
     // -->
 
     @EventHandler
     public void changeClass(ClassChangeEvent event) {
 
-        dEntity hero = new dEntity(event.getHero().getEntity());
-        Player player = null;
+        dHero hero = new dHero(event.getHero());
+        dPlayer player = null;
         dNPC npc = null;
 
         Map<String, dObject> context = new HashMap<String, dObject>();
         context.put("class", new Element(event.getTo().getName()));
         context.put("cost", new Element(event.getCost()));
-        context.put("entity", hero);
+        context.put("from", new Element(event.getFrom().getName()));
+        context.put("hero", hero);
 
         if (hero.isNPC())
             npc = hero.getDenizenNPC();
         else if (hero.isPlayer())
-            player = hero.getPlayer();
+            player = hero.getDenizenPlayer();
 
         String determination = EventManager.doEvents(Arrays.asList
                 ("hero changes class",
                         "hero changes class to " + event.getTo().getName()),
-                npc, new dPlayer(player), context).toUpperCase();
+                npc, player, context).toUpperCase();
 
         if (determination.equals("CANCELLED")) {
             event.setCancelled(true);
@@ -70,28 +75,36 @@ public class HeroesEvents implements Listener {
     // hero changes experience
     // @Triggers when a Hero changes the experience.
     // @Context
-    // <context.amount> returns the amount of experience the Hero gained.
+    // <context.amount> returns the amount the Hero's experience is changing.
+    // <context.class> returns the class that the Hero is changing experience in.
+    // <context.hero> returns the Hero that is changing experience.
+    // <context.reason> returns the reason the Hero is changing experience.
+    // @Determine
+    // "CANCELLED" to stop the hero from gaining experience.
     // @Plugin Heroes
     // -->
 
     @EventHandler
     public void changeExperience(ExperienceChangeEvent event) {
 
-        dEntity hero = new dEntity(event.getHero().getEntity());
-        Player player = null;
+        dHero hero = new dHero(event.getHero());
+        dPlayer player = null;
         dNPC npc = null;
 
         Map<String, dObject> context = new HashMap<String, dObject>();
         context.put("amount", new Element(event.getExpChange()));
+        context.put("class", new Element(event.getHeroClass().getName()));
+        context.put("hero", hero);
+        context.put("reason", new Element(event.getSource().name()));
 
         if (hero.isNPC())
             npc = hero.getDenizenNPC();
         else if (hero.isPlayer())
-            player = hero.getPlayer();
+            player = hero.getDenizenPlayer();
 
         String determination = EventManager.doEvents(Arrays.asList
                 ("hero changes experience"),
-                		npc, new dPlayer(player), context).toUpperCase();
+                		npc, player, context).toUpperCase();
 
         if (determination.equals("CANCELLED")) {
             event.setCancelled(true);
@@ -104,31 +117,42 @@ public class HeroesEvents implements Listener {
     // hero changes level (to <level>)
     // @Triggers when a Hero changes the level.
     // @Context
-    // <context.from> returns the level that the Hero changed from.
-    // <context.class> returns the class that the Hero leveled up with.
+    // <context.class> returns the class that the Hero is changing levels in.
+    // <context.from> returns the level that the Hero is changing from.
+    // <context.hero> returns the Hero that is changing levels.
+    // <context.level> returns the level the Hero is changing to.
+    // @Determine
+    // None
     // @Plugin Heroes
     // -->
 
     @EventHandler
     public void changeLevel(HeroChangeLevelEvent event) {
 
-        dEntity hero = new dEntity(event.getHero().getEntity());
-        Player player = null;
+        dHero hero = new dHero(event.getHero());
+        dPlayer player = null;
         dNPC npc = null;
 
         Map<String, dObject> context = new HashMap<String, dObject>();
-        context.put("from", new Element(event.getFrom()));
         context.put("class", new Element(event.getHeroClass().getName()));
+        context.put("from", new Element(event.getFrom()));
+        context.put("hero", hero);
+        context.put("level", new Element(event.getTo()));
 
         if (hero.isNPC())
             npc = hero.getDenizenNPC();
         else if (hero.isPlayer())
-            player = hero.getPlayer();
+            player = hero.getDenizenPlayer();
 
-        EventManager.doEvents(Arrays.asList
+        String determination = EventManager.doEvents(Arrays.asList
                 	("hero changes level",
                 			"hero changes level to " + event.getTo()),
-                				npc, new dPlayer(player), context).toUpperCase();
+                				npc, player, context).toUpperCase();
+
+        if (determination.equals("CANCELLED")) {
+            // TODO: find a workaround for no event.setCancelled()
+        }
 
     }
+
 }
--- src/main/java/net/gnomeffinway/depenizen/support/HeroesSupport.java
+++ src/main/java/net/gnomeffinway/depenizen/support/HeroesSupport.java
@@ -1,7 +1,10 @@
 package net.gnomeffinway.depenizen.support;
 
+import net.aufdemrand.denizen.objects.ObjectFetcher;
 import net.gnomeffinway.depenizen.Depenizen;
 import net.gnomeffinway.depenizen.events.HeroesEvents;
+import net.gnomeffinway.depenizen.objects.dHero;
+import org.bukkit.Bukkit;
 
 public class HeroesSupport {
 
@@ -13,6 +16,13 @@ public class HeroesSupport {
 
     public void register() {
         new HeroesEvents(depenizen);
+
+        try {
+            ObjectFetcher.registerWithObjectFetcher(dHero.class);
+            ObjectFetcher._initialize();
+        } catch (Exception e) {
+            Bukkit.getLogger().severe("[Depenizen] Error loading Denizen ObjectFetcher. Heroes tags may not function correctly.");
+        }
     }
 
 }
