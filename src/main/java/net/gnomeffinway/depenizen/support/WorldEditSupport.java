e80045e9551e500b077b8b55cf5415431a769e94
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-07-12 15:33:41 -0700

    Add schematic command
    Also remove some scrapfiles.

--- src/main/java/net/gnomeffinway/depenizen/support/WorldEditSupport.java
+++ src/main/java/net/gnomeffinway/depenizen/support/WorldEditSupport.java
@@ -3,6 +3,7 @@ package net.gnomeffinway.depenizen.support;
 import net.aufdemrand.denizen.objects.dPlayer;
 import net.aufdemrand.denizen.utilities.DenizenAPI;
 import net.gnomeffinway.depenizen.Depenizen;
+import net.gnomeffinway.depenizen.commands.SchematicCommand;
 import net.gnomeffinway.depenizen.tags.worldedit.WorldEditPlayerTags;
 
 public class WorldEditSupport {
@@ -15,6 +16,7 @@ public class WorldEditSupport {
 
     public void register() {
         DenizenAPI.getCurrentInstance().getPropertyParser().registerProperty(WorldEditPlayerTags.class, dPlayer.class);
+        new SchematicCommand().activate().as("SCHEMATIC").withOptions("schematic [create/load/unload/rotate/paste/save] [name:<name>] (angle:<#>) (<location>) (<cuboid>) (noair)", 2);
     }
 
 }
