3c89bde049320cf4bdee7e20a53ab4d7cfa722d3
MorphanOne <morphanone@gmail.com>
2014-07-14 23:22:44 -0400

    Add a few Essentials dPlayer tags, mechs, and some events

--- src/main/java/net/gnomeffinway/depenizen/events/EssentialsEvents.java
+++ src/main/java/net/gnomeffinway/depenizen/events/EssentialsEvents.java
@@ -1,10 +1,9 @@
 package net.gnomeffinway.depenizen.events;
 
 import net.aufdemrand.denizen.events.EventManager;
-import net.aufdemrand.denizen.objects.Element;
 import net.aufdemrand.denizen.objects.dObject;
 import net.aufdemrand.denizen.objects.dPlayer;
-import net.ess3.api.events.AfkStatusChangeEvent;
+import net.ess3.api.events.*;
 import net.gnomeffinway.depenizen.Depenizen;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -48,6 +47,129 @@ public class EssentialsEvents implements Listener {
 
         String determination = EventManager.doEvents(events, null,
                 dPlayer.mirrorBukkitPlayer(event.getAffected().getBase()), null).toUpperCase();
+
+        if (determination.equals("CANCELLED"))
+            event.setCancelled(true);
+
+    }
+
+    // <--[event]
+    // @Events
+    // player god mode enabled
+    // @Triggers when a player has god mode enabled.
+    // @Context
+    // <context.controller> returns the dPlayer that enabled god mode for this player, if any.
+    // @Determine
+    // "CANCELLED" to stop the player from having god mode enabled.
+    // @Plugin Essentials
+    // -->
+    // <--[event]
+    // @Events
+    // player god mode disabled
+    // @Triggers when a player has god mode disabled.
+    // @Context
+    // <context.controller> returns the dPlayer that disabled god mode for this player, if any.
+    // @Determine
+    // "CANCELLED" to stop the player from having god mode disabled.
+    // @Plugin Essentials
+    // -->
+    @EventHandler
+    public void godStatusChange(GodStatusChangeEvent event) {
+
+        Map<String, dObject> context = new HashMap<String, dObject>();
+        if (event.getController() != null)
+            context.put("controller", dPlayer.mirrorBukkitPlayer(event.getController().getBase()));
+
+        List<String> events = new ArrayList<String>();
+        if (event.getValue())
+            events.add("player god mode enabled");
+        else
+            events.add("player god mode disabled");
+
+        String determination = EventManager.doEvents(events, null,
+                dPlayer.mirrorBukkitPlayer(event.getAffected().getBase()), context).toUpperCase();
+
+        if (determination.equals("CANCELLED"))
+            event.setCancelled(true);
+
+    }
+
+    // <--[event]
+    // @Events
+    // player jailed
+    // @Triggers when a player is put into jail.
+    // @Context
+    // <context.controller> returns the dPlayer that jailed the player, if any.
+    // @Determine
+    // "CANCELLED" to stop the player from being jailed.
+    // @Plugin Essentials
+    // -->
+    // <--[event]
+    // @Events
+    // player unjailed
+    // @Triggers when a player is taken out of jail.
+    // @Context
+    // <context.controller> returns the dPlayer that unjailed the player, if any.
+    // @Determine
+    // "CANCELLED" to stop the player from being unjailed.
+    // @Plugin Essentials
+    // -->
+    @EventHandler
+    public void jailStatusChange(JailStatusChangeEvent event) {
+
+        Map<String, dObject> context = new HashMap<String, dObject>();
+        if (event.getController() != null)
+            context.put("controller", dPlayer.mirrorBukkitPlayer(event.getController().getBase()));
+
+        List<String> events = new ArrayList<String>();
+        if (event.getValue())
+            events.add("player jailed");
+        else
+            events.add("player unjailed");
+
+        String determination = EventManager.doEvents(events, null,
+                dPlayer.mirrorBukkitPlayer(event.getAffected().getBase()), context).toUpperCase();
+
+        if (determination.equals("CANCELLED"))
+            event.setCancelled(true);
+
+    }
+
+    // <--[event]
+    // @Events
+    // player muted
+    // @Triggers when a player is muted.
+    // @Context
+    // <context.controller> returns the dPlayer that muted the player, if any.
+    // @Determine
+    // "CANCELLED" to stop the player from being muted.
+    // @Plugin Essentials
+    // -->
+    // <--[event]
+    // @Events
+    // player unmuted
+    // @Triggers when a player is unmuted.
+    // @Context
+    // <context.controller> returns the dPlayer that unmuted the player, if any.
+    // @Determine
+    // "CANCELLED" to stop the player from being unmuted.
+    // @Plugin Essentials
+    // -->
+    @EventHandler
+    public void muteStatusChange(MuteStatusChangeEvent event) {
+
+        Map<String, dObject> context = new HashMap<String, dObject>();
+        if (event.getController() != null)
+            context.put("controller", dPlayer.mirrorBukkitPlayer(event.getController().getBase()));
+
+        List<String> events = new ArrayList<String>();
+        if (event.getValue())
+            events.add("player muted");
+        else
+            events.add("player unmuted");
+
+        String determination = EventManager.doEvents(events, null,
+                dPlayer.mirrorBukkitPlayer(event.getAffected().getBase()), context).toUpperCase();
 
         if (determination.equals("CANCELLED"))
             event.setCancelled(true);
--- src/main/java/net/gnomeffinway/depenizen/tags/essentials/EssentialsPlayerTags.java
+++ src/main/java/net/gnomeffinway/depenizen/tags/essentials/EssentialsPlayerTags.java
@@ -1,12 +1,13 @@
 package net.gnomeffinway.depenizen.tags.essentials;
 
-import net.aufdemrand.denizen.objects.Element;
-import net.aufdemrand.denizen.objects.Mechanism;
-import net.aufdemrand.denizen.objects.dObject;
-import net.aufdemrand.denizen.objects.dPlayer;
+import com.earth2me.essentials.User;
+import net.aufdemrand.denizen.objects.*;
 import net.aufdemrand.denizen.objects.properties.Property;
 import net.aufdemrand.denizen.tags.Attribute;
+import net.aufdemrand.denizen.utilities.debugging.dB;
 import net.gnomeffinway.depenizen.Depenizen;
+
+import java.util.GregorianCalendar;
 
 public class EssentialsPlayerTags implements Property {
 
@@ -22,9 +23,9 @@ public class EssentialsPlayerTags implements Property {
     // Instance Fields and Methods
     /////////////
 
-    private EssentialsPlayerTags(dPlayer pl) { this.player = pl; }
+    private EssentialsPlayerTags(dPlayer pl) { this.essUser = Depenizen.essentials.getUser(pl.getName()); }
 
-    dPlayer player = null;
+    User essUser = null;
 
     @Override
     public String getPropertyString() { return null; }
@@ -38,6 +39,28 @@ public class EssentialsPlayerTags implements Property {
     public String getAttribute(Attribute attribute) {
 
         // <--[tag]
+        // @attribute <p@player.god_mode>
+        // @returns Element(Boolean)
+        // @description
+        // Returns whether the player is currently in god mode.
+        // @plugin Essentials
+        // -->
+        if (attribute.startsWith("god_mode")) {
+            return new Element(essUser.isGodModeEnabled()).getAttribute(attribute.fulfill(1));
+        }
+
+        // <--[tag]
+        // @attribute <p@player.has_home>
+        // @returns Element(Boolean)
+        // @description
+        // Returns whether the player has set at least one home.
+        // @plugin Essentials
+        // -->
+        if (attribute.startsWith("has_home")) {
+            return new Element(essUser.hasHome()).getAttribute(attribute.fulfill(1));
+        }
+
+        // <--[tag]
         // @attribute <p@player.is_afk>
         // @returns Element(Boolean)
         // @description
@@ -45,8 +68,97 @@ public class EssentialsPlayerTags implements Property {
         // @plugin Essentials
         // -->
         if (attribute.startsWith("is_afk")) {
-            return new Element(Depenizen.essentials.getUser(player.getName()).isAfk())
+            return new Element(essUser.isAfk()).getAttribute(attribute.fulfill(1));
+        }
+
+        // <--[tag]
+        // @attribute <p@player.is_muted>
+        // @returns Element(Boolean)
+        // @description
+        // Returns whether the player is muted.
+        // @plugin Essentials
+        // -->
+        if (attribute.startsWith("is_muted")) {
+            return new Element(essUser.isMuted()).getAttribute(attribute.fulfill(1));
+        }
+
+        // <--[tag]
+        // @attribute <p@player.list_homes>
+        // @returns dList(Element,dLocation)
+        // @description
+        // Returns a list of the homes of the player, in the format "HomeName,l@x,y,z,world".
+        // @plugin Essentials
+        // -->
+        if (attribute.startsWith("list_homes")) {
+            dList homes = new dList();
+            for (String home : essUser.getHomes()) {
+                try {
+                    homes.add(home + new dLocation(essUser.getHome(home)).identifySimple());
+                } catch (Exception e) {}
+            }
+            return homes.getAttribute(attribute.fulfill(1));
+        }
+
+        // <--[tag]
+        // @attribute <p@player.list_home_locations>
+        // @returns dList(dLocation)
+        // @description
+        // Returns a list of the locations of homes of the player.
+        // @plugin Essentials
+        // -->
+        if (attribute.startsWith("list_homes")) {
+            dList homes = new dList();
+            for (String home : essUser.getHomes()) {
+                try {
+                    homes.add(new dLocation(essUser.getHome(home)).identifySimple());
+                } catch (Exception e) {}
+            }
+            return homes.getAttribute(attribute.fulfill(1));
+        }
+
+        // <--[tag]
+        // @attribute <p@player.list_home_names>
+        // @returns dList(Element)
+        // @description
+        // Returns a list of the names of homes of the player.
+        // @plugin Essentials
+        // -->
+        if (attribute.startsWith("list_home_names")) {
+            return new dList(essUser.getHomes()).getAttribute(attribute.fulfill(1));
+        }
+
+        // <--[tag]
+        // @attribute <p@player.list_mails>
+        // @returns dList(Element)
+        // @description
+        // Returns a list of mail the player currently has.
+        // @plugin Essentials
+        // -->
+        if (attribute.startsWith("list_mails")) {
+            return new dList(essUser.getMails()).getAttribute(attribute.fulfill(1));
+        }
+
+        // <--[tag]
+        // @attribute <p@player.mute_timeout>
+        // @returns Duration
+        // @description
+        // Returns how much time is left until the player is muted.
+        // @plugin Essentials
+        // -->
+        if (attribute.startsWith("mute_timeout")) {
+            return new Duration((int) (essUser.getMuteTimeout() - new GregorianCalendar().getTimeInMillis())/1000)
                     .getAttribute(attribute.fulfill(1));
+        }
+
+        // <--[tag]
+        // @attribute <p@player.socialspy>
+        // @returns Element(Boolean)
+        // @description
+        // Returns whether the player has SocialSpy enabled.
+        // @plugin Essentials
+        // -->
+        if (attribute.startsWith("socialspy")) {
+            return new Element(essUser.isSocialSpyEnabled()).getAttribute(attribute.fulfill(1));
         }
 
         return null;
@@ -54,6 +166,78 @@ public class EssentialsPlayerTags implements Property {
 
     @Override
     public void adjust(Mechanism mechanism) {
+
+        Element value = mechanism.getValue();
+
+        // <--[mechanism]
+        // @object dPlayer
+        // @name afk
+        // @input Element(Boolean)
+        // @description
+        // Sets whether the player is marked as AFK.
+        // @tags
+        // <player.is_afk>
+        // @plugin Essentials
+        // -->
+        if (mechanism.matches("afk") && mechanism.requireBoolean()) {
+            essUser.setAfk(value.asBoolean());
+        }
+
+        // <--[mechanism]
+        // @object dPlayer
+        // @name god_mode
+        // @input Element(Boolean)
+        // @description
+        // Sets whether the player has god mode enabled.
+        // @tags
+        // <player.god_mode>
+        // @plugin Essentials
+        // -->
+        if (mechanism.matches("god_mode") && mechanism.requireBoolean()) {
+            essUser.setGodModeEnabled(value.asBoolean());
+        }
+
+        // <--[mechanism]
+        // @object dPlayer
+        // @name muted
+        // @input Element(Boolean)(|Duration)
+        // @description
+        // Sets whether the player is muted. Optionally, you may also
+        // specify a duration to set how long they are muted for.
+        // @tags
+        // <player.is_muted>
+        // <player.mute_timeout>
+        // @plugin Essentials
+        // -->
+        if (mechanism.matches("muted") && mechanism.requireBoolean()) {
+            if (value.asString().length() > 0) {
+                String[] split = value.asString().split("[\\|" + dList.internal_escape + "]", 2);
+                if (split.length > 0 && new Element(split[0]).isBoolean()) {
+                    essUser.setMuted(new Element(split[0]).asBoolean());
+                    if (split.length > 1 && Duration.matches(split[1])) {
+                        essUser.setMuteTimeout(new GregorianCalendar().getTimeInMillis()
+                                + Duration.valueOf(split[1]).getMillis());
+                    }
+                }
+                else {
+                    dB.echoError("'" + split[0] + "' is not a valid boolean!");
+                }
+            }
+        }
+
+        // <--[mechanism]
+        // @object dPlayer
+        // @name socialspy
+        // @input Element(Boolean)
+        // @description
+        // Sets whether the player has SocialSpy enabled.
+        // @tags
+        // <player.socialspy>
+        // @plugin Essentials
+        // -->
+        if (mechanism.matches("socialspy") && mechanism.requireBoolean()) {
+            essUser.setSocialSpyEnabled(value.asBoolean());
+        }
 
     }
 }
