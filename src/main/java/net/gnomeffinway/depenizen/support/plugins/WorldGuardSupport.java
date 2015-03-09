bca16ee728eb5bb9088181fe62ca3ceb0378b67f
Morphan1 <morphanone@gmail.com>
2015-03-09 01:55:15 -0400

    Add <cu@cuboid.has_region> and <cu@cuboid.regions>
    I still have yet to test much of anything I've added for these plugins..

--- src/main/java/net/gnomeffinway/depenizen/support/plugins/WorldGuardSupport.java
+++ src/main/java/net/gnomeffinway/depenizen/support/plugins/WorldGuardSupport.java
@@ -1,7 +1,9 @@
 package net.gnomeffinway.depenizen.support.plugins;
 
+import net.aufdemrand.denizen.objects.dCuboid;
 import net.aufdemrand.denizen.objects.dLocation;
 import net.gnomeffinway.depenizen.commands.worldguard.RegionCommand;
+import net.gnomeffinway.depenizen.extensions.worldguard.WorldGuardCuboidExtension;
 import net.gnomeffinway.depenizen.objects.worldguard.WorldGuardRegion;
 import net.gnomeffinway.depenizen.support.Support;
 import net.gnomeffinway.depenizen.extensions.worldguard.WorldGuardLocationExtension;
@@ -11,6 +13,7 @@ public class WorldGuardSupport extends Support {
     public WorldGuardSupport() {
         registerObjects(WorldGuardRegion.class);
         registerProperty(WorldGuardLocationExtension.class, dLocation.class);
+        registerProperty(WorldGuardCuboidExtension.class, dCuboid.class);
         new RegionCommand().activate().as("REGION").withOptions("see documentation", 2);
     }
 }
