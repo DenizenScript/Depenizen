3774b529b7306162c2a96e287725f277e393dd25
DNx5 <dani.narianto@gmail.com>
2015-08-23 10:17:41 +0700

    update

--- src/main/java/net/gnomeffinway/depenizen/events/MythicMobsEvents.java
+++ src/main/java/net/gnomeffinway/depenizen/events/MythicMobsEvents.java
@@ -2,10 +2,12 @@ package net.gnomeffinway.depenizen.events;
 
 import net.aufdemrand.denizen.objects.dEntity;
 import net.aufdemrand.denizen.objects.dLocation;
+import net.aufdemrand.denizen.BukkitScriptEntryData;
 import net.aufdemrand.denizencore.events.OldEventManager;
 import net.aufdemrand.denizencore.objects.Element;
 import net.aufdemrand.denizencore.objects.dObject;
 import net.elseland.xikage.MythicMobs.API.Events.MythicMobSpawnEvent;
+import net.elseland.xikage.MythicMobs.API.Events.MythicMobDeathEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -23,13 +25,14 @@ public class MythicMobsEvents implements Listener {
         Map<String, dObject> context = new HashMap<String, dObject>();
         context.put("entity", new dEntity(event.getEntity()));
         context.put("location", new dLocation(event.getLocation()));
+        context.put("mobtype", new Element(mobType));
         context.put("level", new Element(event.getMobLevel()));
 
 
         List<String> determinations = OldEventManager.doEvents(Arrays.asList(
                         "mythicmob spawn",
-                        "mythicmob " + mobType + " spawn"),
-                null, context);
+                        "mythicmob spawn " + mobType),
+                new BukkitScriptEntryData(null, null), context);
 
         for (String determination : determinations) {
             determination = determination.toUpperCase();
@@ -37,4 +40,23 @@ public class MythicMobsEvents implements Listener {
                 event.setCancelled();
         }
     }
+
+    @EventHandler
+    public void onMythicMobDeath(MythicMobDeathEvent event) {
+        String mobType = event.getMobType().MobName;
+
+        Map<String, dObject> context = new HashMap<String, dObject>();
+        context.put("entity", new dEntity(event.getEntity()));
+        context.put("killer", new dEntity(event.getKiller()));
+        context.put("mobtype", new Element(mobType));
+        context.put("level", new Element(event.getMobLevel()));
+        context.put("mobkillcount", new Element(event.getNumberOfPlayerKills()));
+
+
+        OldEventManager.doEvents(Arrays.asList(
+                        "mythicmob death",
+                        "mythicmob death " + mobType),
+                new BukkitScriptEntryData(null, null), context);
+    }
+
 }
