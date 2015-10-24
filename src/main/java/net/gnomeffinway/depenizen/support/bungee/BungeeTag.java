4f6239b03ba58af5958effab17d403932cc2cbde
Talamar1 <me@danielchester.com>
2015-10-23 22:48:45 -0400

    Adding <bungee.server> tag

---
+++ src/main/java/net/gnomeffinway/depenizen/support/bungee/BungeeTag.java
@@ -0,0 +1,36 @@
+package net.gnomeffinway.depenizen.support.bungee;
+
+import net.aufdemrand.denizencore.objects.Element;
+import net.aufdemrand.denizencore.tags.Attribute;
+import net.aufdemrand.denizencore.tags.ReplaceableTagEvent;
+import net.aufdemrand.denizencore.tags.TagManager;
+import net.aufdemrand.denizencore.utilities.CoreUtilities;
+import net.gnomeffinway.depenizen.Settings;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BungeeTag implements Listener {
+
+    public BungeeTag(JavaPlugin depenizen) {
+        depenizen.getServer().getPluginManager().registerEvents(this, depenizen);
+        TagManager.registerTagEvents(this);
+    }
+
+    @TagManager.TagEvents
+    public void bungeeTag(ReplaceableTagEvent event) {
+        if (!event.matches("bungee") || event.replaced()) return;
+        Attribute attribute = event.getAttributes();
+        attribute = attribute.fulfill(1);
+
+        // <--[tag]
+        // @attribute <bungee.server>
+        // @returns Element
+        // @description
+        // returns the current server name (as defined in Depenizen config.yml).
+        // -->
+        if (attribute.startsWith("server")) {
+            event.setReplaced(new Element(CoreUtilities.toLowerCase(Settings.socketName())).getAttribute(attribute.fulfill(1)));
+            return;
+        }
+    }
+}
