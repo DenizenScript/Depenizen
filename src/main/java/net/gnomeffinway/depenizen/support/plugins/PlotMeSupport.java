71a08fef297f9c6865ddc5828586185f10da4410
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-01-02 21:26:40 -0800

    Add placeholders for PlotMe support, for #71

---
+++ src/main/java/net/gnomeffinway/depenizen/support/plugins/PlotMeSupport.java
@@ -0,0 +1,10 @@
+package net.gnomeffinway.depenizen.support.plugins;
+
+import net.gnomeffinway.depenizen.support.Support;
+
+public class PlotMeSupport extends Support {
+
+    public PlotMeSupport() {
+        // TODO: Add support!
+    }
+}
