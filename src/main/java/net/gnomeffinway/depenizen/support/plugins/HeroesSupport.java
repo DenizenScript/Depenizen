b4863e224bb72a78f9d44119769eeeb10cf28f85
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-01-03 22:02:34 -0800

    Actually register heroes events, for #75
    WHO WRITES THIS JUNK

--- src/main/java/net/gnomeffinway/depenizen/support/plugins/HeroesSupport.java
+++ src/main/java/net/gnomeffinway/depenizen/support/plugins/HeroesSupport.java
@@ -2,6 +2,7 @@ package net.gnomeffinway.depenizen.support.plugins;
 
 import net.aufdemrand.denizen.objects.dNPC;
 import net.aufdemrand.denizen.objects.dPlayer;
+import net.gnomeffinway.depenizen.events.HeroesEvents;
 import net.gnomeffinway.depenizen.objects.heroes.HeroesClass;
 import net.gnomeffinway.depenizen.objects.heroes.HeroesHero;
 import net.gnomeffinway.depenizen.support.Support;
@@ -10,6 +11,7 @@ import net.gnomeffinway.depenizen.extensions.heroes.HeroesPlayerNPCExtension;
 public class HeroesSupport extends Support {
 
     public HeroesSupport() {
+        registerEvents(HeroesEvents.class);
         registerObjects(HeroesClass.class, HeroesHero.class);
         registerProperty(HeroesPlayerNPCExtension.class, dNPC.class, dPlayer.class);
     }
