4755ee4cdafc0c75f544fd2f7b0e38e207eef18d
Fortifier42 <fortifier@theausfort.net>
2016-01-08 16:40:42 +1100

    Add forgotten type tags...
    Reformat dTown class... all those unbraced tags D:

--- src/main/java/net/gnomeffinway/depenizen/objects/dTown.java
+++ src/main/java/net/gnomeffinway/depenizen/objects/dTown.java
@@ -5,13 +5,16 @@ import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.exceptions.TownyException;
 import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.TownyUniverse;
-import net.aufdemrand.denizen.objects.*;
+import net.aufdemrand.denizen.objects.dLocation;
+import net.aufdemrand.denizen.objects.dPlayer;
+import net.aufdemrand.denizen.utilities.debugging.dB;
 import net.aufdemrand.denizencore.objects.Element;
 import net.aufdemrand.denizencore.objects.Fetchable;
+import net.aufdemrand.denizencore.objects.dList;
 import net.aufdemrand.denizencore.objects.dObject;
 import net.aufdemrand.denizencore.tags.Attribute;
-import net.aufdemrand.denizen.utilities.debugging.dB;
 import net.aufdemrand.denizencore.tags.TagContext;
+import org.bukkit.Location;
 
 public class dTown implements dObject {
 
@@ -25,7 +28,9 @@ public class dTown implements dObject {
 
     @Fetchable("town")
     public static dTown valueOf(String string, TagContext context) {
-        if (string == null) return null;
+        if (string == null) {
+            return null;
+        }
 
         ////////
         // Match town name
@@ -110,8 +115,9 @@ public class dTown implements dObject {
             try {
                 return new Element(town.getHoldingBalance()).getAttribute(attribute.fulfill(1));
             } catch (EconomyException e) {
-                if (!attribute.hasAlternative())
+                if (!attribute.hasAlternative()) {
                     dB.echoError("Invalid economy response!");
+                }
             }
         }
 
@@ -122,9 +128,10 @@ public class dTown implements dObject {
         // Returns the town's current board.
         // @plugin Depenizen, Towny
         // -->
-        else if (attribute.startsWith("board"))
+        else if (attribute.startsWith("board")) {
             return new Element(town.getTownBoard())
                     .getAttribute(attribute.fulfill(1));
+        }
 
         // <--[tag]
         // @attribute <town@town.is_open>
@@ -133,9 +140,10 @@ public class dTown implements dObject {
         // Returns true if the town is currently open.
         // @plugin Depenizen, Towny
         // -->
-        else if (attribute.startsWith("isopen") || attribute.startsWith("is_open"))
+        else if (attribute.startsWith("isopen") || attribute.startsWith("is_open")) {
             return new Element(town.isOpen())
                     .getAttribute(attribute.fulfill(1));
+        }
 
         // <--[tag]
         // @attribute <town@town.is_public>
@@ -144,9 +152,10 @@ public class dTown implements dObject {
         // Returns true if the town is currently public.
         // @plugin Depenizen, Towny
         // -->
-        else if (attribute.startsWith("ispublic") || attribute.startsWith("is_public"))
+        else if (attribute.startsWith("ispublic") || attribute.startsWith("is_public")) {
             return new Element(town.isPublic())
                     .getAttribute(attribute.fulfill(1));
+        }
 
         // <--[tag]
         // @attribute <town@town.mayor>
@@ -155,9 +164,10 @@ public class dTown implements dObject {
         // Returns the mayor of the town.
         // @plugin Depenizen, Towny
         // -->
-        else if (attribute.startsWith("mayor"))
+        else if (attribute.startsWith("mayor")) {
             return dPlayer.valueOf(town.getMayor().getName())
                     .getAttribute(attribute.fulfill(1));
+        }
 
         // <--[tag]
         // @attribute <town@town.name>
@@ -166,9 +176,10 @@ public class dTown implements dObject {
         // Returns the town's names.
         // @plugin Depenizen, Towny
         // -->
-        else if (attribute.startsWith("name"))
+        else if (attribute.startsWith("name")) {
             return new Element(town.getName())
                     .getAttribute(attribute.fulfill(1));
+        }
 
         // <--[tag]
         // @attribute <town@town.nation>
@@ -177,7 +188,7 @@ public class dTown implements dObject {
         // Returns the nation that the town belongs to.
         // @plugin Depenizen, Towny
         // -->
-        else if (attribute.startsWith("nation"))
+        else if (attribute.startsWith("nation")) {
             try {
                 return new dNation(town.getNation())
                         .getAttribute(attribute.fulfill(1));
@@ -190,9 +201,10 @@ public class dTown implements dObject {
         // Returns the number of players in the town.
         // @plugin Depenizen, Towny
         // -->
-        else if (attribute.startsWith("playercount") || attribute.startsWith("player_count"))
+        else if (attribute.startsWith("playercount") || attribute.startsWith("player_count")) {
             return new Element(town.getNumResidents())
                     .getAttribute(attribute.fulfill(1));
+        }
 
         // <--[tag]
         // @attribute <town@town.size>
@@ -201,9 +213,10 @@ public class dTown implements dObject {
         // Returns the number of blocks the town owns.
         // @plugin Depenizen, Towny
         // -->
-        else if (attribute.startsWith("size"))
+        else if (attribute.startsWith("size")) {
             return new Element(town.getPurchasedBlocks())
                     .getAttribute(attribute.fulfill(1));
+        }
 
         // <--[tag]
         // @attribute <town@town.spawn>
@@ -226,9 +239,10 @@ public class dTown implements dObject {
         // Returns the town's tag.
         // @plugin Depenizen, Towny
         // -->
-        else if (attribute.startsWith("tag"))
+        else if (attribute.startsWith("tag")) {
             return new Element(town.getTag())
                     .getAttribute(attribute.fulfill(1));
+        }
 
         // <--[tag]
         // @attribute <town@town.taxes>
@@ -237,9 +251,25 @@ public class dTown implements dObject {
         // Returns the town's current taxes.
         // @plugin Depenizen, Towny
         // -->
-        else if (attribute.startsWith("taxes"))
+        else if (attribute.startsWith("taxes")) {
             return new Element(town.getTaxes())
                     .getAttribute(attribute.fulfill(1));
+        }
+
+        // <--[tag]
+        // @attribute <town@town.outposts>
+        // @returns dList(dLocation)
+        // @description
+        // Returns a list of the town's outpost locations.
+        // @plugin Depenizen, Towny
+        // -->
+        else if (attribute.startsWith("outposts")) {
+            dList posts = new dList();
+            for (Location p : town.getAllOutpostSpawns()) {
+                posts.add(new dLocation(p).identify());
+            }
+            return posts.getAttribute(attribute.fulfill(1));
+        }
 
         // <--[tag]
         // @attribute <town@town.type>
