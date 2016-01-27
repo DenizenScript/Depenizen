77aff576b8ff3103873a27669c5c79d56d5e5146
Fortifier42 <fortifier@theausfort.net>
2016-01-27 12:57:20 +1100

    Add region fetching <region[id,world]>

--- src/main/java/net/gnomeffinway/depenizen/support/plugins/WorldGuardSupport.java
+++ src/main/java/net/gnomeffinway/depenizen/support/plugins/WorldGuardSupport.java
@@ -2,6 +2,7 @@ package net.gnomeffinway.depenizen.support.plugins;
 
 import net.aufdemrand.denizen.objects.dCuboid;
 import net.aufdemrand.denizen.objects.dLocation;
+import net.aufdemrand.denizencore.tags.Attribute;
 import net.gnomeffinway.depenizen.commands.worldguard.RegionCommand;
 import net.gnomeffinway.depenizen.extensions.worldguard.WorldGuardCuboidExtension;
 import net.gnomeffinway.depenizen.objects.worldguard.WorldGuardRegion;
@@ -15,5 +16,22 @@ public class WorldGuardSupport extends Support {
         registerProperty(WorldGuardLocationExtension.class, dLocation.class);
         registerProperty(WorldGuardCuboidExtension.class, dCuboid.class);
         new RegionCommand().activate().as("REGION").withOptions("see documentation", 2);
+        registerAdditionalTags("region");
     }
+
+    @Override
+    public String additionalTags(Attribute attribute) {
+        if (attribute == null) {
+            return null;
+        }
+        if (attribute.startsWith("region") && attribute.hasContext(1)) {
+            WorldGuardRegion region = WorldGuardRegion.valueOf(attribute.getContext(1));
+            if (region == null) {
+                return null;
+            }
+            return region.getAttribute(attribute.fulfill(1));
+        }
+        return null;
+    }
+
 }
