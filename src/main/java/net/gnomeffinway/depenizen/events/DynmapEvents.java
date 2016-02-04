0d0e6c726f93c69ba3faddcbf7d54fb52590d9a8
Fortifier42 <fortifier@theausfort.net>
2016-02-04 17:34:06 +1100

    Remove useless DynMap support...

--- src/main/java/net/gnomeffinway/depenizen/events/DynmapEvents.java
+++
@@ -1,9 +0,0 @@
-package net.gnomeffinway.depenizen.events;
-
-import org.bukkit.event.Listener;
-
-public class DynmapEvents implements Listener {
-
-    // Will be automatically registered, does not need constructor.
-    // TODO: add events
-}
