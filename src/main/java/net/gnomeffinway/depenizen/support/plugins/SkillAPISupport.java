828bdab4e010597372b260412de7da9ddaae8413
Morphan1 <morphanone@gmail.com>
2016-02-11 16:23:45 -0500

    Add SkillAPI tags, fixes #98

--- src/main/java/net/gnomeffinway/depenizen/support/plugins/SkillAPISupport.java
+++ src/main/java/net/gnomeffinway/depenizen/support/plugins/SkillAPISupport.java
@@ -1,11 +1,16 @@
 package net.gnomeffinway.depenizen.support.plugins;
 
+import net.aufdemrand.denizen.objects.dPlayer;
 import net.gnomeffinway.depenizen.events.SkillAPIEvents;
+import net.gnomeffinway.depenizen.extensions.skillapi.SkillAPIPlayerExtension;
+import net.gnomeffinway.depenizen.objects.skillapi.SkillAPIClass;
 import net.gnomeffinway.depenizen.support.Support;
 
 public class SkillAPISupport extends Support {
 
     public SkillAPISupport() {
+        registerObjects(SkillAPIClass.class);
+        registerProperty(SkillAPIPlayerExtension.class, dPlayer.class);
         registerEvents(SkillAPIEvents.class);
     }
 
