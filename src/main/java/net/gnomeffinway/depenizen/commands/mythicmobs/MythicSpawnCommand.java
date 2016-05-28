89cec52402915e4660d582733ee9fd7b02b4ad96
Morphan1 <morphanone@gmail.com>
2016-05-28 17:39:01 -0400

    Add level to MythicSpawn command

--- src/main/java/net/gnomeffinway/depenizen/commands/mythicmobs/MythicSpawnCommand.java
+++ src/main/java/net/gnomeffinway/depenizen/commands/mythicmobs/MythicSpawnCommand.java
@@ -10,12 +10,14 @@ import net.aufdemrand.denizencore.scripts.commands.AbstractCommand;
 import net.elseland.xikage.MythicMobs.API.IMobsAPI;
 import net.elseland.xikage.MythicMobs.Mobs.MythicMob;
 import net.elseland.xikage.MythicMobs.MythicMobs;
+import net.gnomeffinway.depenizen.objects.mythicmobs.MythicMobsMob;
+import org.bukkit.entity.Entity;
 
 public class MythicSpawnCommand extends AbstractCommand {
 
     // <--[command]
     // @Name MythicSpawn
-    // @Syntax mythicspawn [<name>] [<location>]
+    // @Syntax mythicspawn [<name>] [<location>] (level:<#>)
     // @Group Depenizen
     // @Plugin Depenizen, MythicMobs
     // @Required 2
@@ -27,7 +29,7 @@ public class MythicSpawnCommand extends AbstractCommand {
     // This allows you to spawn a MythicMob at a location using the mob's internal name.
 
     // @Tags
-    // <entry[saveName].spawned_mythicmob>
+    // <entry[saveName].spawned_mythicmob> returns the spawned MythicMobsMob.
 
     // @Usage
     // Use to spawn a BarbarianMinion at a player's location.
@@ -45,6 +47,12 @@ public class MythicSpawnCommand extends AbstractCommand {
                 scriptEntry.addObject("location", arg.asType(dLocation.class));
             }
 
+            else if (!scriptEntry.hasObject("level")
+                    && arg.matchesPrefix("level", "l")
+                    && arg.matchesPrimitive(aH.PrimitiveType.Integer)) {
+                scriptEntry.addObject("level", arg.asElement());
+            }
+
             else if (!scriptEntry.hasObject("name")) {
                 scriptEntry.addObject("name", arg.asElement());
             }
@@ -58,6 +66,8 @@ public class MythicSpawnCommand extends AbstractCommand {
         if (!scriptEntry.hasObject("location") || !scriptEntry.hasObject("name")) {
             throw new InvalidArgumentsException("Must specify a name and location.");
         }
+
+        scriptEntry.defaultObject("level", new Element(1));
     }
 
     @Override
@@ -65,12 +75,14 @@ public class MythicSpawnCommand extends AbstractCommand {
 
         Element name = scriptEntry.getElement("name");
         dLocation location = scriptEntry.getdObject("location");
+        Element level = scriptEntry.getElement("level");
 
         IMobsAPI api = MythicMobs.inst().getAPI().getMobAPI();
 
         try {
             MythicMob mob = api.getMythicMob(name.asString());
-            api.spawnMythicMob(mob, location);
+            Entity entity = api.spawnMythicMob(mob, location, level.asInt());
+            scriptEntry.addObject("spawned_mythicmob", new MythicMobsMob(api.getMythicMobInstance(entity)));
         }
         catch (Exception e) {
             e.printStackTrace();
