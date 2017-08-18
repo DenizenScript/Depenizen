65887f0232ea95df5a7b5e6ea62082876d734e22
Morphan1 <morphanone@gmail.com>
2017-08-17 20:42:28 -0400

    Temporarily disable this event for now

--- bukkit/src/main/java/com/denizenscript/depenizen/bukkit/support/plugins/McMMOSupport.java
+++ bukkit/src/main/java/com/denizenscript/depenizen/bukkit/support/plugins/McMMOSupport.java
@@ -2,14 +2,13 @@ package com.denizenscript.depenizen.bukkit.support.plugins;
 
 import com.denizenscript.depenizen.bukkit.commands.McMMOCommands;
 import com.denizenscript.depenizen.bukkit.events.mcmmo.mcMMOPlayerGainsXPScriptEvent;
-import com.denizenscript.depenizen.bukkit.events.mcmmo.mcMMOPlayerLevelChangeScriptEvent;
 import com.denizenscript.depenizen.bukkit.events.mcmmo.mcMMOPlayerLevelDownScriptEvent;
 import com.denizenscript.depenizen.bukkit.events.mcmmo.mcMMOPlayerLevelUpScriptEvent;
 import com.denizenscript.depenizen.bukkit.extensions.mcmmo.McMMOPlayerExtension;
 import com.denizenscript.depenizen.bukkit.objects.dParty;
+import com.denizenscript.depenizen.bukkit.support.Support;
 import net.aufdemrand.denizen.objects.dPlayer;
 import net.aufdemrand.denizencore.tags.Attribute;
-import com.denizenscript.depenizen.bukkit.support.Support;
 
 public class McMMOSupport extends Support {
 
@@ -18,7 +17,7 @@ public class McMMOSupport extends Support {
         registerAdditionalTags("party");
         registerProperty(McMMOPlayerExtension.class, dPlayer.class);
         new McMMOCommands().activate().as("MCMMO").withOptions("See Documentation.", 1);
-        registerScriptEvents(new mcMMOPlayerLevelChangeScriptEvent());
+        //registerScriptEvents(new mcMMOPlayerLevelChangeScriptEvent()); TODO: re-enable when mcMMO fixes it
         registerScriptEvents(new mcMMOPlayerLevelUpScriptEvent());
         registerScriptEvents(new mcMMOPlayerLevelDownScriptEvent());
         registerScriptEvents(new mcMMOPlayerGainsXPScriptEvent());
