51dd5f73783709c03cced4d34abca41c22997fa6
Alex "mcmonkey" Goodwin <agoodwin@freneticllc.com>
2019-06-16 20:01:20 -0700

    repatch start: remove sponge files

--- sponge/src/main/java-templates/com/denizenscript/depenizen/sponge/PomData.java
+++
@@ -1,6 +0,0 @@
-package com.denizenscript.depenizen.sponge;
-
-public class PomData {
-    public static final String VERSION = "${version}";
-    public static final String BUILD_NUMBER = "${BUILD_NUMBER}";
-}
