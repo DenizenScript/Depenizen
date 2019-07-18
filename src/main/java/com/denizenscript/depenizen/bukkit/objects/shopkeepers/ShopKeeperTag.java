38d82b43ecd28d8916d8f9580ec626c5d11a5d7b
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-07-17 23:57:11 -0700

    update for new Denizen build compat

--- src/main/java/com/denizenscript/depenizen/bukkit/objects/shopkeepers/ShopKeeperTag.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/objects/shopkeepers/ShopKeeperTag.java
@@ -14,7 +14,7 @@ import com.denizenscript.denizencore.objects.core.ListTag;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.tags.Attribute;
 import com.denizenscript.denizencore.tags.TagContext;
-import com.denizenscript.denizencore.tags.core.EscapeTags;
+import com.denizenscript.denizencore.tags.core.EscapeTagBase;
 import com.denizenscript.denizencore.utilities.debugging.Debug;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
@@ -172,7 +172,7 @@ public class ShopKeeperTag implements ObjectTag {
             ListTag trades = new ListTag();
             for (TradingRecipe trade : shopkeeper.getTradingRecipes(null)) {
                 ListTag recipe = wrapTradingRecipe(trade);
-                trades.add(EscapeTags.escape(recipe.identify()));
+                trades.add(EscapeTagBase.escape(recipe.identify()));
             }
             return trades.getAttribute(attribute.fulfill(1));
         }
