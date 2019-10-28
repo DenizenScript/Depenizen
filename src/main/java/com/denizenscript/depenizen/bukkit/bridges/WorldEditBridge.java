a718be2ef00f07ab672070ebf4b7af7e42dbd042
Mwthorn <mwthorn@gmail.com>
2019-10-28 19:06:17 +0100

    WorldEdit commands for clipboard/schematics (#277)
    * Added WE Command
    
    * change args in WE command

--- src/main/java/com/denizenscript/depenizen/bukkit/bridges/WorldEditBridge.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/bridges/WorldEditBridge.java
@@ -1,7 +1,9 @@
 package com.denizenscript.depenizen.bukkit.bridges;
 
+import com.denizenscript.denizen.utilities.DenizenAPI;
 import com.denizenscript.depenizen.bukkit.Bridge;
 import com.denizenscript.denizen.objects.PlayerTag;
+import com.denizenscript.depenizen.bukkit.commands.worldedit.WorldEditCommand;
 import com.denizenscript.depenizen.bukkit.properties.worldedit.WorldEditPlayerProperties;
 import com.denizenscript.denizencore.objects.properties.PropertyParser;
 
@@ -12,6 +14,8 @@ public class WorldEditBridge extends Bridge {
     @Override
     public void init() {
         instance = this;
+        DenizenAPI.getCurrentInstance().getCommandRegistry().registerCoreMember(WorldEditCommand.class,
+                "WORLDEDIT", "worldedit [create_schematic/copy_to_clipboard/paste] (file:<file path>) (cuboid:<cuboid>) (position:<location>) (rotate:<#>) (undoable) (noair)", 2);
         PropertyParser.registerProperty(WorldEditPlayerProperties.class, PlayerTag.class);
     }
 }
