b1b9c4ec6348ab0f2e6b38c4523b06bbe8f234a8
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-01-08 13:20:44 -0800

    workaround Shopkeeper sub-event being implemented wrongly

--- src/main/java/com/denizenscript/depenizen/bukkit/events/shopkeepers/ShopKeeperCreatedScriptEvent.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/events/shopkeepers/ShopKeeperCreatedScriptEvent.java
@@ -6,6 +6,7 @@ import com.denizenscript.denizen.objects.PlayerTag;
 import com.denizenscript.denizen.utilities.implementation.BukkitScriptEntryData;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.scripts.ScriptEntryData;
+import com.nisovin.shopkeepers.api.events.PlayerCreatePlayerShopkeeperEvent;
 import com.nisovin.shopkeepers.api.events.PlayerCreateShopkeeperEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -65,5 +66,10 @@ public class ShopKeeperCreatedScriptEvent extends BukkitScriptEvent implements L
         player = new PlayerTag(event.getShopCreationData().getCreator());
         this.event = event;
         fire(event);
+    }
+
+    @EventHandler
+    public void onPlayerShopKeeperCreated(PlayerCreatePlayerShopkeeperEvent event) {
+        onShopKeeperCreated(event);
     }
 }
