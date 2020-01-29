4d8dac283de7431a4809376bb1c566921c8a030f
PikaMug <2267126+PikaMug@users.noreply.github.com>
2020-01-28 22:15:35 -0500

    Wrap debug report See https://github.com/DenizenScript/Depenizen/pull/305#discussion_r371785922

--- src/main/java/com/denizenscript/depenizen/bukkit/commands/quests/QuestsCommand.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/commands/quests/QuestsCommand.java
@@ -126,11 +126,13 @@ public class QuestsCommand extends AbstractCommand {
 
         PlayerTag player = scriptEntryData.getPlayer();
 
-        Debug.report(scriptEntry, getName(), action.debug() 
-                + (questId != null ? questId.debug() : "") 
-                + (stageNum != null ? stageNum.debug() : "") 
-                + (points != null ? points.debug() : "") 
-                + (state != null ? state.debug() : ""));
+        if (scriptEntry.dbCallShouldDebug()) {
+            Debug.report(scriptEntry, getName(), action.debug() 
+                    + (questId != null ? questId.debug() : "") 
+                    + (stageNum != null ? stageNum.debug() : "") 
+                    + (points != null ? points.debug() : "") 
+                    + (state != null ? state.debug() : ""));
+        }
 
         switch (Action.valueOf(action.asString().toUpperCase())) {
 
