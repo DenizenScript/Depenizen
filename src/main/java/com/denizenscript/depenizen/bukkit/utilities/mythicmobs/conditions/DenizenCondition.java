0a5bd839d4d96752286c636be48b6732649bd5a3
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2022-02-03 04:42:31 -0800

    mm 'DenizenCondition' cleanup

--- src/main/java/com/denizenscript/depenizen/bukkit/utilities/mythicmobs/conditions/DenizenCondition.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/utilities/mythicmobs/conditions/DenizenCondition.java
@@ -3,8 +3,8 @@ package com.denizenscript.depenizen.bukkit.utilities.mythicmobs.conditions;
 import com.denizenscript.denizen.objects.EntityTag;
 import com.denizenscript.denizen.objects.LocationTag;
 import com.denizenscript.denizencore.objects.ObjectTag;
-import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.scripts.queues.ContextSource;
+import com.denizenscript.denizencore.tags.ParseableTag;
 import com.denizenscript.denizencore.tags.TagContext;
 import com.denizenscript.denizencore.tags.TagManager;
 import com.denizenscript.denizencore.utilities.CoreUtilities;
@@ -21,44 +21,56 @@ import java.util.HashMap;
 
 public class DenizenCondition extends SkillCondition implements IEntityCondition, ILocationCondition, ICasterCondition, ISkillMetaCondition, IEntityComparisonCondition {
 
-    final String tag;
-    ContextSource.SimpleMap source;
+    public ParseableTag tag;
+    public ContextSource.SimpleMap source;
 
     public DenizenCondition(String line, MythicLineConfig mlc) {
         super(line);
-        tag = mlc.getString("tag");
+        tag = TagManager.parseTextToTag(mlc.getString("tag"), CoreUtilities.noDebugContext);
         source = new ContextSource.SimpleMap();
         source.contexts = new HashMap<>();
     }
 
+    public void reset() {
+        source.contexts.clear();
+    }
+
     @Override
     public boolean check(AbstractEntity entity) {
+        reset();
         source.contexts.put("target", new EntityTag(entity.getBukkitEntity()));
         return runCheck();
     }
 
     @Override
     public boolean check(AbstractLocation location) {
+        reset();
         source.contexts.put("location", new LocationTag((World) location.getWorld(), location.getX(), location.getY(), location.getZ()));
         return runCheck();
     }
 
+    public void addContext(SkillCaster caster) {
+        source.contexts.put("entity", new EntityTag(caster.getEntity().getBukkitEntity()));
+        AbstractEntity target = caster.getEntity().getTarget();
+        if (target != null) {
+            source.contexts.put("target", new EntityTag(target.getBukkitEntity()));
+        }
+    }
+
     @Override
     public boolean check(SkillCaster caster) {
-        source.contexts.put("entity", new EntityTag(caster.getEntity().getBukkitEntity()));
-        if (caster.getEntity().getTarget() == null) {
-            source.contexts.put("target", new EntityTag(caster.getEntity().getTarget().getBukkitEntity()));
-        }
+        reset();
+        addContext(caster);
         return runCheck();
     }
 
     @Override
     public boolean check(SkillMetadata skillMetadata) {
-        source.contexts.put("entity", new EntityTag(skillMetadata.getCaster().getEntity().getBukkitEntity()));
-        source.contexts.put("target", new EntityTag(skillMetadata.getCaster().getEntity().getTarget().getBukkitEntity()));
+        reset();
         if (skillMetadata.getTrigger() == null) {
             source.contexts.put("trigger", new EntityTag(skillMetadata.getTrigger().getBukkitEntity()));
         }
+        addContext(skillMetadata.getCaster());
         return runCheck();
     }
 
@@ -72,7 +84,7 @@ public class DenizenCondition extends SkillCondition implements IEntityCondition
     public boolean runCheck() {
         TagContext tagContext = CoreUtilities.noDebugContext.clone();
         tagContext.contextSource = source;
-        ObjectTag object = TagManager.tagObject(tag, tagContext);
+        ObjectTag object = tag.parse(tagContext);
         return object.asElement().asBoolean();
     }
 }
