3c73c8f4b26ae3a7d892c4221686e0dc938b4a6a
Aya <31237389+tal5@users.noreply.github.com>
2022-05-09 09:29:16 +0100

    WIP `DataSerializer`

---
+++ src/main/java/com/denizenscript/depenizen/bukkit/clientizen/DataSerializer.java
@@ -0,0 +1,60 @@
+package com.denizenscript.depenizen.bukkit.clientizen;
+
+import com.denizenscript.denizencore.utilities.debugging.Debug;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Map;
+
+public class DataSerializer {
+
+    public DataOutput output;
+    public ByteArrayOutputStream outputStream;
+
+    public DataSerializer() {
+        outputStream = new ByteArrayOutputStream();
+        output = new DataOutputStream(outputStream);
+    }
+
+    public void writeInt(int i) {
+        try {
+            output.writeInt(i);
+        }
+        catch (IOException e) {
+            Debug.echoError(new IllegalStateException(e));
+        }
+    }
+
+    public void writeByteArray(@NotNull byte[] bytes) {
+        try {
+            output.write(bytes);
+        }
+        catch (IOException e) {
+            Debug.echoError(new IllegalStateException(e));
+        }
+    }
+
+    public void writeString(@NotNull String s) {
+        writeByteArray(s.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public void writeStringList(@NotNull Collection<String> strings) {
+        writeInt(strings.size());
+        for (String s : strings) {
+            writeString(s);
+        }
+    }
+
+    public void writeStringListMap(@NotNull Map<String, Collection<String>> map) {
+        writeInt(map.size());
+        for (Map.Entry<String, Collection<String>> entry : map.entrySet()) {
+            writeString(entry.getKey());
+            writeStringList(entry.getValue());
+        }
+    }
+}
