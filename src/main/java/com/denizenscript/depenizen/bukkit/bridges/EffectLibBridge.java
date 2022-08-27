c5c5a1d769b853d4cf362338d3777cb7e0e6877a
Aya <31237389+tal5@users.noreply.github.com>
2022-08-27 15:18:39 +0300

    Rework EffectLib Integration (#384)

--- src/main/java/com/denizenscript/depenizen/bukkit/bridges/EffectLibBridge.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/bridges/EffectLibBridge.java
@@ -1,16 +1,20 @@
 package com.denizenscript.depenizen.bukkit.bridges;
 
 import com.denizenscript.denizencore.DenizenCore;
+import com.denizenscript.depenizen.bukkit.Depenizen;
 import com.denizenscript.depenizen.bukkit.commands.effectlib.EffectLibCommand;
 import com.denizenscript.depenizen.bukkit.Bridge;
+import de.slikey.effectlib.EffectManager;
 
 public class EffectLibBridge extends Bridge {
 
     public static EffectLibBridge instance;
+    public EffectManager effectManager;
 
     @Override
     public void init() {
         instance = this;
+        effectManager = new EffectManager(Depenizen.instance);
         DenizenCore.commandRegistry.registerCommand(EffectLibCommand.class);
     }
 }
