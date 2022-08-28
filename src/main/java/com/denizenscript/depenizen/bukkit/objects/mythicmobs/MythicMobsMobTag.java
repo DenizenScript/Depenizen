26fa462e3ea8ab5928c6178c135117399df74498
0tickpulse <0tickpulse@gmail.com>
2022-08-29 07:39:47 +0800

    new mythic variable tags/mechs (#385)
    * new mythic variable tags/mechs
    
    * no google dependency, stray newlines, tag format
    
    * var name consistency, removed unused imports
    
    * more unused dependencies because im dumb
    
    * oops heres another one
    
    * removed another double nl and wrong comment
    
    * mechanism comments

--- src/main/java/com/denizenscript/depenizen/bukkit/objects/mythicmobs/MythicMobsMobTag.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/objects/mythicmobs/MythicMobsMobTag.java
@@ -6,6 +6,7 @@ import com.denizenscript.denizencore.objects.core.DurationTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.core.MapTag;
 import com.denizenscript.denizencore.tags.ObjectTagProcessor;
+import com.denizenscript.denizencore.utilities.text.StringHolder;
 import com.denizenscript.depenizen.bukkit.bridges.MythicMobsBridge;
 import com.denizenscript.denizen.objects.EntityTag;
 import com.denizenscript.denizencore.tags.Attribute;
@@ -16,9 +17,12 @@ import io.lumine.mythic.api.adapters.AbstractEntity;
 import io.lumine.mythic.api.mobs.MythicMob;
 import io.lumine.mythic.bukkit.adapters.BukkitEntity;
 import io.lumine.mythic.core.mobs.ActiveMob;
+import io.lumine.mythic.core.skills.variables.Variable;
+import io.lumine.mythic.core.skills.variables.VariableType;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -356,6 +360,33 @@ public class MythicMobsMobTag implements ObjectTag, Adjustable {
         tagProcessor.registerTag(DurationTag.class, "global_cooldown", (attribute, object) -> {
             return new DurationTag(object.getMob().getGlobalCooldown());
         });
+
+        // <--[tag]
+        // @attribute <MythicMobsMobTag.mythic_variable[<name>]>
+        // @returns ElementTag
+        // @plugin Depenizen, MythicMobs
+        // @description
+        // Returns the value of a MythicMobs variable for this MythicMob.
+        // -->
+        tagProcessor.registerTag(ElementTag.class, ElementTag.class, "mythic_variable", (attribute, object, secondVal) -> {
+            return new ElementTag(MythicMobsBridge.getMythicVariable(object.getEntity(), secondVal.asString()), true);
+        });
+
+        // <--[tag]
+        // @attribute <MythicMobsMobTag.mythic_variable_map>
+        // @returns MapTag
+        // @plugin Depenizen, MythicMobs
+        // @mechanism MythicMobsMobTag.mythic_variable_map
+        // @description
+        // Returns a map of the MythicMob's variables.
+        // -->
+        tagProcessor.registerTag(MapTag.class, "mythic_variable_map", (attribute, object) -> {
+            MapTag result = new MapTag();
+            for (Map.Entry<String, Variable> entry : MythicMobsBridge.getMythicVariableMap(object.getEntity()).entrySet()) {
+                result.putObject(entry.getKey(), new ElementTag(entry.getValue().toString(), true));
+            }
+            return result;
+        });
     }
 
     @Override
@@ -365,6 +396,25 @@ public class MythicMobsMobTag implements ObjectTag, Adjustable {
 
     @Override
     public void adjust(Mechanism mechanism) {
+
+        // <--[mechanism]
+        // @object MythicMobsMobTag
+        // @name mythic_variable_map
+        // @input MapTag
+        // @plugin Depenizen, MythicMobs
+        // @description
+        // Sets the variables of a MythicMob from a map.
+        // @tags
+        // <MythicMobsMobTag.mythic_variable_map>
+        // -->
+        if (mechanism.matches("mythic_variable_map") && mechanism.requireObject(MapTag.class)) {
+            MapTag map = mechanism.valueAsType(MapTag.class);
+            Map<String, Variable> newMap = new HashMap<String, Variable>();
+            for (Map.Entry<StringHolder, ObjectTag> entry : map.map.entrySet()) {
+                newMap.put(entry.getKey().str, Variable.ofType(VariableType.STRING, entry.getValue()));
+            }
+            MythicMobsBridge.setMythicVariableMap(mob.getEntity().getBukkitEntity(), newMap);
+        }
 
         // <--[mechanism]
         // @object MythicMobsMobTag
---
+++ src/main/java/com/denizenscript/depenizen/bukkit/properties/mythicmobs/MythicMobsPlayerProperties.java
@@ -0,0 +1,105 @@
+package com.denizenscript.depenizen.bukkit.properties.mythicmobs;
+
+import com.denizenscript.denizen.objects.PlayerTag;
+import com.denizenscript.denizencore.objects.core.MapTag;
+import com.denizenscript.denizencore.objects.properties.PropertyParser;
+import com.denizenscript.denizencore.utilities.text.StringHolder;
+import com.denizenscript.depenizen.bukkit.bridges.MythicMobsBridge;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.denizenscript.denizencore.objects.ObjectTag;
+import com.denizenscript.denizencore.objects.properties.Property;
+import com.denizenscript.denizencore.objects.Mechanism;
+import io.lumine.mythic.core.skills.variables.Variable;
+import io.lumine.mythic.core.skills.variables.VariableType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MythicMobsPlayerProperties implements Property {
+
+    @Override
+    public String getPropertyString() {
+        return null;
+    }
+
+    @Override
+    public String getPropertyId() {
+        return "MythicMobsPlayer";
+    }
+
+    public static boolean describes(ObjectTag object) {
+        return object instanceof PlayerTag;
+    }
+
+    public static MythicMobsPlayerProperties getFrom(ObjectTag object) {
+        if (!describes(object)) {
+            return null;
+        }
+        else {
+            return new MythicMobsPlayerProperties((PlayerTag) object);
+        }
+    }
+
+    public static final String[] handledMechs = new String[] {
+            "mythic_variable_map"
+    };
+
+    public MythicMobsPlayerProperties(PlayerTag player) {
+        this.player = player;
+    }
+
+    PlayerTag player;
+
+    public static void registerTags() {
+
+        // <--[tag]
+        // @attribute <PlayerTag.mythic_variable[<name>]>
+        // @returns ElementTag
+        // @plugin Depenizen, MythicMobs
+        // @description
+        // Returns the value of a MythicMobs variable for this MythicMob.
+        // -->
+        PropertyParser.registerTag(MythicMobsPlayerProperties.class, ElementTag.class, "mythic_variable", (attribute, object) -> {
+            return new ElementTag(MythicMobsBridge.getMythicVariable(object.player.getPlayerEntity(), attribute.getParam()), true);
+        });
+
+        // <--[tag]
+        // @attribute <PlayerTag.mythic_variable_map>
+        // @returns MapTag
+        // @plugin Depenizen, MythicMobs
+        // @mechanism PlayerTag.mythic_variable_map
+        // @description
+        // Returns a map of the MythicMob's variables.
+        // -->
+        PropertyParser.registerTag(MythicMobsPlayerProperties.class, MapTag.class, "mythic_variable_map", (attribute, object) -> {
+            MapTag result = new MapTag();
+            for (Map.Entry<String, Variable> entry : MythicMobsBridge.getMythicVariableMap(object.player.getPlayerEntity()).entrySet()) {
+                result.putObject(entry.getKey(), new ElementTag(entry.getValue().toString(), true));
+            }
+            return result;
+        });
+    }
+
+    @Override
+    public void adjust(Mechanism mechanism) {
+
+        // <--[mechanism]
+        // @object PlayerTag
+        // @name mythic_variable_map
+        // @input MapTag
+        // @plugin Depenizen, MythicMobs
+        // @description
+        // Sets the variables of a player from a map.
+        // @tags
+        // <PlayerTag.mythic_variable_map>
+        // -->
+        if (mechanism.matches("mythic_variable_map") && mechanism.requireObject(MapTag.class)) {
+            MapTag map = mechanism.valueAsType(MapTag.class);
+            Map<String, Variable> newMap = new HashMap<String, Variable>();
+            for (Map.Entry<StringHolder, ObjectTag> entry : map.map.entrySet()) {
+                newMap.put(entry.getKey().str, Variable.ofType(VariableType.STRING, entry.getValue()));
+            }
+            MythicMobsBridge.setMythicVariableMap(player.getPlayerEntity(), newMap);
+        }
+    }
+}
