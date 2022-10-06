a6a320367dd0c67b78e8be68bb745e956c82755c
nstJames <james.castillo@nessotech.com>
2022-10-06 13:24:03 +0800

    change max args and added default object

--- src/main/java/com/denizenscript/depenizen/bukkit/commands/mythicmobs/MythicSpawnCommand.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/commands/mythicmobs/MythicSpawnCommand.java
@@ -10,6 +10,7 @@ import com.denizenscript.denizencore.scripts.commands.AbstractCommand;
 import com.denizenscript.denizencore.utilities.debugging.Debug;
 import com.denizenscript.depenizen.bukkit.objects.mythicmobs.MythicMobsMobTag;
 import io.lumine.mythic.api.mobs.MythicMob;
+import io.lumine.mythic.api.mobs.entities.SpawnReason;
 import io.lumine.mythic.bukkit.BukkitAdapter;
 import org.bukkit.entity.Entity;
 
@@ -27,14 +28,11 @@ public class MythicSpawnCommand extends AbstractCommand {
     // @Group Depenizen
     // @Plugin Depenizen, MythicMobs
     // @Required 2
-    // @Maximum 3
+    // @Maximum 4
     // @Short Spawns a MythicMob at a location.
     //
     // @Description
     // This allows you to spawn a MythicMob at a location using the mob's internal name.
-    //
-    // Optionally specify 'reason:<reason>' (Paper only) to specify the reason an entity is spawning for the 'entity spawns' event,
-    // using any reason from https://hub.spigotmc.org/javadocs/spigot/org/bukkit/event/entity/CreatureSpawnEvent.SpawnReason.html
     //
     // @Tags
     // <entry[saveName].spawned_mythicmob> returns the spawned MythicMobsMob.
@@ -70,6 +68,7 @@ public class MythicSpawnCommand extends AbstractCommand {
             throw new InvalidArgumentsException("Must specify a name and location.");
         }
         scriptEntry.defaultObject("level", new ElementTag(1));
+        scriptEntry.defaultObject("reason", new ElementTag("OTHER"));
     }
 
     @Override
