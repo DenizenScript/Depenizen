be9e3acd89f2877c38742bf36506a65027e8d6e1
Servlet <31308819+MadServlet@users.noreply.github.com>
2022-11-16 16:27:45 +0800

    add towny join and leave event (#395)
    * add towny join and leave event
    
    * applied reviews
    corrected grammatical error in documentation
    changed switch-case to one line
    removed redundant new lines
    added space after `-->` documentation
    removed explicit cancellable in documentation

---
+++ src/main/java/com/denizenscript/depenizen/bukkit/events/towny/PlayerLeavesTownScriptEvent.java
@@ -0,0 +1,55 @@
+package com.denizenscript.depenizen.bukkit.events.towny;
+
+import com.denizenscript.denizen.events.BukkitScriptEvent;
+import com.denizenscript.denizen.utilities.implementation.BukkitScriptEntryData;
+import com.denizenscript.denizencore.objects.ObjectTag;
+import com.denizenscript.denizencore.scripts.ScriptEntryData;
+import com.denizenscript.depenizen.bukkit.objects.towny.TownTag;
+import com.palmergames.bukkit.towny.event.TownRemoveResidentEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class PlayerLeavesTownScriptEvent extends BukkitScriptEvent implements Listener {
+
+    // <--[event]
+    // @Events
+    // towny player leaves town
+    //
+    // @Triggers when a player is removed from a Towny town or when a Towny town is dissolved.
+    //
+    // @Context
+    // <context.town> returns the town the player belongs to.
+    //
+    // @Plugin Depenizen, Towny
+    //
+    // @Player Always.
+    //
+    // @Group Depenizen
+    //
+    // -->
+
+    public PlayerLeavesTownScriptEvent() {
+        registerCouldMatcher("towny player leaves town");
+    }
+
+    public TownRemoveResidentEvent event;
+
+    @Override
+    public ScriptEntryData getScriptEntryData() {
+        return new BukkitScriptEntryData(event.getResident().getPlayer());
+    }
+
+    @Override
+    public ObjectTag getContext(String name) {
+        switch (name) {
+            case "town": return new TownTag(event.getTown());
+        }
+        return super.getContext(name);
+    }
+
+    @EventHandler
+    public void onPlayerJoinTown(TownRemoveResidentEvent event) {
+        this.event = event;
+        fire(event);
+    }
+}
