f9bae5491f3687a8ee7a9241fbcc40480d4fd1a9
david <89749147+analnyroztahovac@users.noreply.github.com>
2022-11-18 20:16:03 +0100

    added ResidenceRaidStarts/Ends event

---
+++ src/main/java/com/denizenscript/depenizen/bukkit/events/residence/ResidenceRaidEndsScriptEvent.java
@@ -0,0 +1,65 @@
+package com.denizenscript.depenizen.bukkit.events.residence;
+
+import com.bekvon.bukkit.residence.event.ResidenceRaidEndEvent;
+import com.bekvon.bukkit.residence.protection.ClaimedResidence;
+import com.denizenscript.denizencore.objects.ObjectTag;
+import com.denizenscript.depenizen.bukkit.objects.residence.ResidenceTag;
+import com.denizenscript.denizen.events.BukkitScriptEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+
+public class ResidenceRaidEndsScriptEvent extends BukkitScriptEvent implements Listener {
+
+    // <--[event]
+    // @Events
+    // residence raid end
+    // residence raid ends
+    //
+    // @Switch residence:<residence_name> to only process the event if the residence name equals specified name
+    //
+    // @Triggers when a raiding a Residence ends.
+    //
+    // @Context
+    // <context.residence> Returns a ResidenceTag of residence that was being raided.
+    //
+    // @Plugin Depenizen, Residence
+    //
+    // @Group Depenizen
+    //
+    // -->
+
+    public ResidenceRaidEndsScriptEvent() {
+        registerCouldMatcher("residence raid ends"); registerCouldMatcher("residence raid end");
+        registerSwitches("residence");
+    }
+
+    public ResidenceRaidEndEvent event;
+    public ClaimedResidence residence;
+
+    @Override
+    public boolean matches(ScriptPath path) {
+        if (!runGenericSwitchCheck(path, "residence", residence.getName())) {
+            return false;
+        }
+        return super.matches(path);
+    }
+
+    @Override
+    public ObjectTag getContext(String name) {
+        if (name.equals("residence")) {
+            return new ResidenceTag(residence);
+        }
+        return super.getContext(name);
+    }
+
+    @EventHandler
+    public void onResidenceRaidEndsScriptEvent(ResidenceRaidEndEvent event) {
+        if (event.getRes() == null) {
+            return;
+        }
+        residence = event.getRes();
+        this.event = event;
+        fire(event);
+    }
+}
---
+++ src/main/java/com/denizenscript/depenizen/bukkit/events/residence/ResidenceRaidStartsScriptEvent.java
@@ -0,0 +1,85 @@
+package com.denizenscript.depenizen.bukkit.events.residence;
+
+import com.bekvon.bukkit.residence.event.ResidenceRaidStartEvent;
+import com.bekvon.bukkit.residence.raid.RaidAttacker;
+import com.bekvon.bukkit.residence.raid.RaidDefender;
+import com.denizenscript.denizen.objects.PlayerTag;
+import com.denizenscript.denizencore.objects.ObjectTag;
+import com.denizenscript.denizencore.objects.core.ListTag;
+import com.denizenscript.depenizen.bukkit.objects.residence.ResidenceTag;
+import com.denizenscript.denizen.events.BukkitScriptEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+
+public class ResidenceRaidStartsScriptEvent extends BukkitScriptEvent implements Listener {
+
+    // <--[event]
+    // @Events
+    // residence raid start
+    // residence raid starts
+    //
+    // @Switch residence:<residence_name> to only process the event if the residence name equals specified name
+    //
+    // @Triggers when a player(s) starts raiding a Residence.
+    //
+    // @Context
+    // <context.residence> Returns a ResidenceTag of residence that is being attacked.
+    // <context.defenders> Returns a ListTag(PlayerTag) of players defending the Residence.
+    // <context.attackers> Returns a ListTag(PlayerTag) of players attacking the Residence.
+    //
+    // @Plugin Depenizen, Residence
+    //
+    // @Group Depenizen
+    //
+    // -->
+
+    public ResidenceRaidStartsScriptEvent() {
+        registerCouldMatcher("residence raid starts"); registerCouldMatcher("residence raid start");
+        registerSwitches("residence");
+    }
+
+    public ResidenceRaidStartEvent event;
+    public ResidenceTag residence;
+    public ListTag attackers;
+    public ListTag defenders;
+
+    @Override
+    public boolean matches(ScriptPath path) {
+        if (!runGenericSwitchCheck(path, "residence", event.getRes().getName())) {
+            return false;
+        }
+        return super.matches(path);
+    }
+
+    @Override
+    public ObjectTag getContext(String name) {
+        switch (name) {
+            case "attackers":
+                attackers = new ListTag();
+                for (RaidAttacker p : event.getAttackers().values()) {
+                    attackers.addObject(new PlayerTag(p.getPlayer().getPlayer()));
+                }
+                return attackers;
+            case "defenders":
+                defenders = new ListTag();
+                for (RaidDefender p : event.getDefenders().values()) {
+                    defenders.addObject(new PlayerTag(p.getPlayer().getPlayer()));
+                }
+                return defenders;
+            case "residence":
+                return new ResidenceTag(event.getRes());
+            default:
+                return super.getContext(name);
+        }
+    }
+
+    @EventHandler
+    public void onResidenceRaidStartsScriptEvent(ResidenceRaidStartEvent event) {
+        if (event.getRes() == null) {
+            return;
+        }
+        this.event = event;
+        fire(event);
+    }
+}
