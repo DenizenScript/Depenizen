c37d96a5e836434ca74b569a8bb6da9764d031e8
david <89749147+analnyroztahovac@users.noreply.github.com>
2023-08-26 16:31:02 +0200

    ViaVersion bridge update (#418)
    * update registration, add new tag
    
    * lib bump
    
    * note
    
    * fixes
    
    * typo

--- src/main/java/com/denizenscript/depenizen/bukkit/bridges/ViaVersionBridge.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/bridges/ViaVersionBridge.java
@@ -1,14 +1,12 @@
 package com.denizenscript.depenizen.bukkit.bridges;
 
-import com.denizenscript.denizen.objects.PlayerTag;
-import com.denizenscript.denizencore.objects.properties.PropertyParser;
 import com.denizenscript.depenizen.bukkit.Bridge;
-import com.denizenscript.depenizen.bukkit.properties.viaversion.ViaVersionPlayerProperties;
+import com.denizenscript.depenizen.bukkit.properties.viaversion.ViaVersionPlayerExtensions;
 
 public class ViaVersionBridge extends Bridge {
 
     @Override
     public void init() {
-        PropertyParser.registerProperty(ViaVersionPlayerProperties.class, PlayerTag.class);
+        ViaVersionPlayerExtensions.register();
     }
 }
---
+++ src/main/java/com/denizenscript/depenizen/bukkit/properties/viaversion/ViaVersionPlayerExtensions.java
@@ -0,0 +1,48 @@
+package com.denizenscript.depenizen.bukkit.properties.viaversion;
+
+import com.denizenscript.denizen.objects.PlayerTag;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.viaversion.viaversion.api.Via;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+
+public class ViaVersionPlayerExtensions {
+
+    public static void register() {
+
+        // <--[tag]
+        // @attribute <PlayerTag.viaversion>
+        // @returns ElementTag(Number)
+        // @plugin Depenizen, ViaVersion
+        // @deprecated Use 'PlayerTag.viaversion_protocol'
+        // @description
+        // Deprecated in favor of <@link tag PlayerTag.viaversion_protocol>.
+        // -->
+
+        // <--[tag]
+        // @attribute <PlayerTag.viaversion_protocol>
+        // @returns ElementTag(Number)
+        // @plugin Depenizen, ViaVersion
+        // @description
+        // Returns the protocol version number of the player's client.
+        // See <@link url https://wiki.vg/Protocol_version_numbers> as a reference list.
+        // See also <@link tag PlayerTag.viaversion_version>
+        // -->
+        PlayerTag.registerOnlineOnlyTag(ElementTag.class, "viaversion_protocol", (attribute, object) -> {
+            return new ElementTag(Via.getAPI().getPlayerVersion(object.getUUID()));
+        }, "viaversion");
+
+        // <--[tag]
+        // @attribute <PlayerTag.viaversion_version>
+        // @returns ElementTag
+        // @plugin Depenizen, ViaVersion
+        // @description
+        // Returns the player's client version ("1.19.4", "1.18.2"...).
+        // See also <@link tag PlayerTag.viaversion_protocol>
+        // -->
+        PlayerTag.registerOnlineOnlyTag(ElementTag.class, "viaversion_version", (attribute, object) -> {
+            int version = Via.getAPI().getPlayerVersion(object.getUUID());
+            return new ElementTag(ProtocolVersion.getProtocol(version).getName(), true);
+        });
+    }
+
+}
