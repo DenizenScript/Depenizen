c7159d40afb54bf55b5d976affc80cc6502c9d44
david <89749147+davight@users.noreply.github.com>
2025-04-13 01:28:51 +0200

    Worldguard properties update (#441)
    * fix while not having citizens installed
    
    * convert to extensions
    
    * convert to extensions (location)
    
    * convert to extensions (world)
    
    * add extension (player)
    
    * convert to extensions (player)
    
    * update
    
    * meta fixes
    
    * more meta fixes
    
    * remove pointless deprecations
    
    * fixes

---
+++ src/main/java/com/denizenscript/depenizen/bukkit/properties/worldguard/WorldGuardCuboidExtensions.java
@@ -0,0 +1,55 @@
+package com.denizenscript.depenizen.bukkit.properties.worldguard;
+
+import com.denizenscript.denizen.objects.CuboidTag;
+import com.denizenscript.denizen.objects.LocationTag;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.denizenscript.denizencore.objects.core.ListTag;
+import com.denizenscript.depenizen.bukkit.objects.worldguard.WorldGuardRegionTag;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+
+public class WorldGuardCuboidExtensions {
+
+    public static ApplicableRegionSet getApplicableRegions(CuboidTag cuboid) {
+        LocationTag low = cuboid.getLow(0);
+        LocationTag high = cuboid.getHigh(0);
+        BlockVector3 vecLow = BlockVector3.at(low.getX(), low.getY(), low.getZ());
+        BlockVector3 vecHigh = BlockVector3.at(high.getX(), high.getY(), high.getZ());
+        ProtectedCuboidRegion region = new ProtectedCuboidRegion("FAKE_REGION", vecLow, vecHigh);
+        return WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(cuboid.getWorld().getWorld())).getApplicableRegions(region);
+    }
+
+    public static void register() {
+
+        // <--[tag]
+        // @attribute <CuboidTag.has_region>
+        // @returns ElementTag(Boolean)
+        // @plugin Depenizen, WorldGuard
+        // @description
+        // Returns whether the cuboid contains any region.
+        // -->
+        CuboidTag.tagProcessor.registerTag(ElementTag.class, "has_region", (attribute, area) -> {
+            return new ElementTag(getApplicableRegions(area).size() > 0);
+        });
+
+        // <--[tag]
+        // @attribute <CuboidTag.regions>
+        // @returns ListTag(WorldGuardRegionTag)
+        // @plugin Depenizen, WorldGuard
+        // @description
+        // Returns a list of regions that are in this cuboid.
+        // -->
+        CuboidTag.tagProcessor.registerTag(ListTag.class, "regions", (attribute, area) -> {
+            ListTag regionList = new ListTag();
+            for (ProtectedRegion protectedRegion : getApplicableRegions(area)) {
+                regionList.addObject(new WorldGuardRegionTag(protectedRegion, area.getWorld().getWorld()));
+            }
+            return regionList;
+        });
+    }
+
+}
