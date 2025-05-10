d3988fabe3611be229dcf4021748432e0b3dba4c
MCSamuel <morrissamuelw@gmail.com>
2025-05-10 10:58:00 -0700

    helper method added

--- src/main/java/com/denizenscript/depenizen/bukkit/properties/superiorskyblock/SuperiorSkyblockPlayerExtensions.java
+++ src/main/java/com/denizenscript/depenizen/bukkit/properties/superiorskyblock/SuperiorSkyblockPlayerExtensions.java
@@ -3,6 +3,7 @@ package com.denizenscript.depenizen.bukkit.properties.superiorskyblock;
 import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
 import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.api.island.PlayerRole;
+import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import com.denizenscript.denizen.objects.PlayerTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.depenizen.bukkit.objects.superiorskyblock.SuperiorSkyblockIslandTag;
@@ -15,6 +16,10 @@ public class SuperiorSkyblockPlayerExtensions {
 
     PlayerTag player;
 
+    public static SuperiorPlayer getSuperiorPlayer(PlayerTag player) {
+        return SuperiorSkyblockAPI.getPlayer(player.getUUID());
+    }
+
     public static void register() {
 
         // <--[tag]
@@ -26,7 +31,7 @@ public class SuperiorSkyblockPlayerExtensions {
         // Returns whether a player can build on islands that they are not a part of.
         // -->
         PlayerTag.tagProcessor.registerTag(ElementTag.class, "superiorskyblock_bypass_mode", (attribute, player) -> {
-            return new ElementTag(SuperiorSkyblockAPI.getPlayer(player.getUUID()).hasBypassModeEnabled());
+            return new ElementTag(getSuperiorPlayer(player).hasBypassModeEnabled());
         });
 
         // <--[tag]
@@ -38,7 +43,7 @@ public class SuperiorSkyblockPlayerExtensions {
         // Returns the amount of disbands a player has left.
         // -->
         PlayerTag.tagProcessor.registerTag(ElementTag.class, "superiorskyblock_disbands", (attribute, player) -> {
-            return new ElementTag(SuperiorSkyblockAPI.getPlayer(player.getUUID()).getDisbands());
+            return new ElementTag(getSuperiorPlayer(player).getDisbands());
         });
 
         // <--[tag]
@@ -49,7 +54,7 @@ public class SuperiorSkyblockPlayerExtensions {
         // Returns the island a player belongs to, if any.
         // -->
         PlayerTag.tagProcessor.registerTag(SuperiorSkyblockIslandTag.class, "superiorskyblock_island", (attribute, player) -> {
-            Island island = SuperiorSkyblockAPI.getPlayer(player.getUUID()).getIsland();
+            Island island = getSuperiorPlayer(player).getIsland();
             if (island != null) {
                 return new SuperiorSkyblockIslandTag(island);
             }
@@ -64,7 +69,7 @@ public class SuperiorSkyblockPlayerExtensions {
         // Returns the role a player has on an island, if any.
         // -->
         PlayerTag.tagProcessor.registerTag(ElementTag.class, "superiorskyblock_island_role", (attribute, player) -> {
-            PlayerRole role = SuperiorSkyblockAPI.getPlayer(player.getUUID()).getPlayerRole();
+            PlayerRole role = getSuperiorPlayer(player).getPlayerRole();
             if (role != null) {
                 return new ElementTag(role.getName(), true);
             }
@@ -80,7 +85,7 @@ public class SuperiorSkyblockPlayerExtensions {
         // Returns whether a player can see team chat from all islands.
         // -->
         PlayerTag.tagProcessor.registerTag(ElementTag.class, "superiorskyblock_spy_mode", (attribute, player) -> {
-            return new ElementTag(SuperiorSkyblockAPI.getPlayer(player.getUUID()).hasAdminSpyEnabled());
+            return new ElementTag(getSuperiorPlayer(player).hasAdminSpyEnabled());
         });
 
         // <--[mechanism]
@@ -95,7 +100,7 @@ public class SuperiorSkyblockPlayerExtensions {
         // -->
         PlayerTag.registerOnlineOnlyMechanism("superiorskyblock_bypass_mode", ElementTag.class, (player, mechanism, value) -> {
             if (mechanism.requireBoolean()) {
-                SuperiorSkyblockAPI.getPlayer(player.getUUID()).setBypassMode(mechanism.getValue().asBoolean());
+                getSuperiorPlayer(player).setBypassMode(mechanism.getValue().asBoolean());
             }
         });
 
@@ -111,7 +116,7 @@ public class SuperiorSkyblockPlayerExtensions {
         // -->
         PlayerTag.registerOnlineOnlyMechanism("superiorskyblock_disbands", ElementTag.class, (player, mechanism, value) -> {
             if (mechanism.requireInteger()) {
-                SuperiorSkyblockAPI.getPlayer(player.getUUID()).setDisbands(mechanism.getValue().asInt());
+                getSuperiorPlayer(player).setDisbands(mechanism.getValue().asInt());
             }
         });
 
@@ -127,7 +132,7 @@ public class SuperiorSkyblockPlayerExtensions {
         // -->
         PlayerTag.registerOnlineOnlyMechanism("superiorskyblock_spy_mode", ElementTag.class, (player, mechanism, value) -> {
             if (mechanism.requireBoolean()) {
-                SuperiorSkyblockAPI.getPlayer(player.getUUID()).setAdminSpy(mechanism.getValue().asBoolean());
+                getSuperiorPlayer(player).setAdminSpy(mechanism.getValue().asBoolean());
             }
         });
     }
